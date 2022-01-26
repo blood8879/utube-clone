6fc06ffb70ef02c3032241b53173817390ea8de1
blood8879 <blood8879@gmail.com>
2022-01-26 22:19:19 +0900

    videoDetail fin.

--- server/routes/video.js
+++ server/routes/video.js
@@ -48,6 +48,24 @@ router.post('/uploadVideo', (req, res) => {
     }) 
 })
 
+router.get('/getVideos', (req, res) => {
+    Video.find()
+        .populate('writer')
+        .exec((err, videos) => {
+            if(err) return res.status(400).send(err);
+            res.status(200).json({ success: true, videos })
+        })
+})
+
+router.post('/getVideoDetail', (req, res) => {
+    Video.findOne({ "_id" : req.body.videoId })
+        .populate('writer')
+        .exec((err, videoDetail) => {
+            if(err) return res.status(400).send(err)
+            return res.status(200).json({ success: true, videoDetail })
+        })
+})
+
 router.post('/thumbnail', (req, res) => {
 
     let filePath = ""
